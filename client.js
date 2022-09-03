88d35e9899d38634c742399eafb54db7611b7ca5
Alireza <alirezatlb45@gmail.com>
2022-09-03 16:32:08 +0430

    Initialized socket in client and server

--- client.js
+++ client.js
@@ -1,1 +1,8 @@
-const socket = new WebSocket()
+const socket = io("http://localhost:3000");
+
+socket.on("connect", (data) => {
+    socket.emit("welcome-server", "hello Server!!!i am a front developer");
+    socket.on("welcom-client", data => {
+        console.log(data)
+    })
+})
---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const socketIO = require("socket.io");
+
+const server = http.createServer();
+
+const io = socketIO(server, {
+    cors : {
+        origin : "*"
+    } 
+});
+
+io.on("connection", (socket) => {
+    socket.on("welcome-server", (data) => {
+        console.log(data)
+    });
+
+    socket.emit("welcom-client", "hello client!!! i am a backend developer")
+})
+
+server.listen(3000, () => { console.log("The server is running on port 3000") })
